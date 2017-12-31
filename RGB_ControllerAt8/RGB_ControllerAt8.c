1fba62eb80b29cd73b724d7b48c4e22df204c978
Dawid Nowacki <dawid.nowacki7@gmail.com>
2017-12-31 13:49:27 +0100

    Add wave mode
    Modulate brightness with set frequency

--- RGB_ControllerAt8/RGB_ControllerAt8.c
+++ RGB_ControllerAt8/RGB_ControllerAt8.c
@@ -19,11 +19,12 @@
 #define LED_DDR DDRC
 
 volatile uint8_t pwm_GREEN=255, pwm_BLUE=255, pwm_RED=255; //definicje zmiennych przechowuj¹cych wartoœc pwm
+volatile uint8_t tmp_GREEN = 0, tmp_BLUE=0, tmp_RED=0; 
 volatile char inputbuff[40];
-volatile int freq = 0, cnt2 = 0;
-volatile char mix = 0;
-volatile char onOff = 0;
-volatile uint8_t cntMix = 0, cnt =0;
+int freq = 0, cnt2 = 0;
+char mix = 0, wave = 0;
+char onOff = 0;
+uint8_t cntMix = 0, cnt = 0;
 
 ISR(TIMER1_COMPA_vect) 
 {
@@ -45,29 +46,27 @@ ISR(TIMER1_COMPA_vect)
 			cnt = 0;
 	}
 	else
-		LED_PORT &= ~(GREEN | BLUE | RED);	
+		LED_PORT &= ~(GREEN | BLUE | RED);			
 	cnt++;	
 }  
 
 void blik()
 {
-	if(freq>0)
+	
+	if(cnt2<freq)	
+		onOff = 1;	
+	else
 	{
-		if(cnt2<freq)
-		onOff = 1;
+		if(cnt2 < 2*freq)		
+			onOff = 0;		
 		else
-		{
-			if(cnt2 < 2*freq)
-			onOff = 0;
-			else
-			{
-				cnt2 = 0;
-				if(mix == 1)
-				mixSet();
-			}
-		}
-		cnt2++;
+		{	
+			cnt2 = 0;
+			if(mix == 1)
+				mixSet();	
+		}			
 	}
+	cnt2++;	
 }
 
 void mixSet()
@@ -95,6 +94,66 @@ void mixSet()
 	cntMix++;
 }
 
+
+void blikWave()
+{	
+	static char upDown = 0;
+	if(cnt2>freq)
+	{		
+		if(upDown == 0 && mix == 0)
+		{		
+			if(pwm_BLUE>0)	
+				pwm_BLUE--;
+			if(pwm_GREEN>0)
+				pwm_GREEN--;
+			if(pwm_RED>0)
+				pwm_RED--;
+			if((pwm_BLUE+pwm_GREEN+pwm_RED) == 0)
+				upDown = 1;
+		}
+		
+			
+		if(upDown == 1 && mix == 0)
+		{
+			if(pwm_BLUE<tmp_BLUE)
+				pwm_BLUE++;
+			if(pwm_GREEN<tmp_GREEN)
+				pwm_GREEN++;
+			if(pwm_RED<tmp_RED)
+				pwm_RED++;
+			if(pwm_BLUE+pwm_GREEN+pwm_RED == tmp_BLUE+tmp_GREEN+tmp_RED)
+				upDown = 0;
+		}			
+		cnt2 = 0;
+		if(mix == 1)
+			mixSetWave();		
+	}
+	cnt2++;
+}
+
+void mixSetWave()
+{
+	static char upDownM = 1;
+	if(pwm_RED<255 && upDownM == 1)
+		pwm_RED++;			
+	if(pwm_RED == 255 && pwm_GREEN<255 && upDownM == 1)
+		pwm_GREEN++;			
+	if(pwm_RED == 255 && pwm_GREEN==255 && pwm_BLUE<255 && upDownM == 1)		
+		pwm_BLUE++;
+	if(pwm_RED == 255 && pwm_GREEN==255 && pwm_BLUE==255 && upDownM == 1)
+		upDownM = 0;
+		
+	if(pwm_RED>0 && upDownM == 0)
+		pwm_RED--;
+	if(pwm_RED == 0 && pwm_GREEN>0 && upDownM == 0)
+		pwm_GREEN--;
+	if(pwm_RED == 0 && pwm_GREEN==0 && pwm_BLUE>0 && upDownM == 0)
+		pwm_BLUE--;
+	if(pwm_RED == 0 && pwm_GREEN==0 && pwm_BLUE==0 && upDownM == 0)
+		upDownM = 1;
+	
+}
+
 void CTC1_init() //inciclalizacja timera1  
 {		
 	TCCR1B |= (1 << WGM12)|(1 << CS11)|(1 << CS10);
@@ -106,8 +165,8 @@ void CTC1_init() //inciclalizacja timera1
 char checkReceived()
 {
 	if(inputbuff[0]== 'S' && inputbuff[1]== 'E' && inputbuff[2]== 'T')	
-		if(inputbuff[4]== 'T'&& inputbuff[7]== 'R' && inputbuff[12]== 'G' && inputbuff[17]== 'B'&& inputbuff[22]== 'F'&& inputbuff[28]== 'M')
-			if(inputbuff[31]== 'E' && inputbuff[32]== 'N' && inputbuff[33]== 'D')
+		if(inputbuff[4]== 'T'&& inputbuff[7]== 'R' && inputbuff[12]== 'G' && inputbuff[17]== 'B'&& inputbuff[22]== 'F'&& inputbuff[28]== 'M'&& inputbuff[31]== 'W')
+			if(inputbuff[34]== 'E' && inputbuff[35]== 'N' && inputbuff[36]== 'D')
 				return 0;
 	return 1;
 }
@@ -127,9 +186,9 @@ int main(void)
 	while(1) 
 	{    
 		receivedData = uart_available();	
-		if(receivedData >= 34)
+		if(receivedData >= 37)
 		{			
-			for(int i = 0; i < 34; i++)
+			for(int i = 0; i < 37; i++)
 			{
 				aa = uart_getc();				
 				inputbuff[i]= aa & 0xFF;	
@@ -139,11 +198,12 @@ int main(void)
 			{
 				cli();
 				onOff = inputbuff[5]-0x30;
-				pwm_RED = (inputbuff[8]-0x30)*100+(inputbuff[9]-0x30)*10+(inputbuff[10]-0x30);
-				pwm_GREEN = (inputbuff[13]-0x30)*100+(inputbuff[14]-0x30)*10+(inputbuff[15]-0x30);
-				pwm_BLUE = (inputbuff[18]-0x30)*100+(inputbuff[19]-0x30)*10+(inputbuff[20]-0x30);
+				pwm_RED = tmp_RED= (inputbuff[8]-0x30)*100+(inputbuff[9]-0x30)*10+(inputbuff[10]-0x30);
+				pwm_GREEN = tmp_GREEN = (inputbuff[13]-0x30)*100+(inputbuff[14]-0x30)*10+(inputbuff[15]-0x30);
+				pwm_BLUE = tmp_BLUE =(inputbuff[18]-0x30)*100+(inputbuff[19]-0x30)*10+(inputbuff[20]-0x30);
 				freq = (inputbuff[23]-0x30)*1000+(inputbuff[24]-0x30)*100+(inputbuff[25]-0x30)*10 +(inputbuff[26]-0x30);
 				mix = inputbuff[29]-0x30;
+				wave = inputbuff[32]-0x30;
 				sei();			
 				uart_puts("ACK\r\n");				
 			}
@@ -151,8 +211,11 @@ int main(void)
 				uart_puts("NACK\r\n");
 			uart_flush();																						
 		}
-		if(cnt % 20 == 0)
-			blik();	   		
+		if(cnt % 20 == 0 && freq > 0)
+			if(wave != 1)
+				blik();
+			else
+				blikWave();	   		
 	} 
 	return 0; 
 }
